409f86b64b671a07c5f9fe375737eb47b93dbe91
liuyu <liuyu12345@qq.com>
2019-08-07 16:12:28 +0800

    helper for lua

---
+++ Assets/Scripts/tilemap/TileMapGenHelperForLua.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+public class TileMapGenHelperForLua
+{
+    static TileMapGenBase m_TileMapGen;
+
+    public static void Init(Transform terrainRoot, object terrainInfo, int v_count, int h_count, int itemEx_x, int itemEx_y, float offset_x, float offset_y){
+        m_TileMapGen = terrainRoot.GetComponent<TileMapGenBase>();
+        if (m_TileMapGen)
+        {
+            var data = terrainInfo as TerrainInfo;
+            m_TileMapGen.Init(data, v_count, h_count, itemEx_x, itemEx_y, offset_x, offset_y);
+        }
+    }
+    public static void SetResPath(string tileMapInfoPath, string tileMapInfoName, string itemInfoPath, string itemInfoName, string terrainObjPath, string terrainObjName, string itemObjPath){
+        m_TileMapGen.SetResPath(tileMapInfoPath, tileMapInfoName, itemInfoPath, itemInfoName, terrainObjPath, terrainObjName, itemObjPath);
+    }
+    public static void MoveTo(float x, float z){
+        m_TileMapGen.MoveTo(x, z);
+    }
+
+}
